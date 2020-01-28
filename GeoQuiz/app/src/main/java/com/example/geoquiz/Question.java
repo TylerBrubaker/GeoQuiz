98a7ddfbdc5fa157c3a91aa00585d9470c42a603
Tyler <tabrubaker@my.harrisburgu.edu>
2020-01-28 16:49:59 -0500

    Uploaded application data.

---
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/Question.java
@@ -0,0 +1,27 @@
+package com.example.geoquiz;
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int mTextResId) {
+        this.mTextResId = mTextResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean mAnswerTrue) {
+        this.mAnswerTrue = mAnswerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+}
